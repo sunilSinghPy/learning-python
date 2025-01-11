a7928ca424021914c870eaff4c1fe94d5b0b4134
sunilSinghPy <coolsunilsingh@gmail.com>
2025-01-11 22:03:05 +0530

    hangman updated

--- hangman.py
+++ hangman.py
@@ -4,22 +4,30 @@ word = random.choice(words)
 print(word)
 #word_char_list = list(word)
 number_of_attampt = 10
-print_blank = '_'
-print_blank = print_blank * len(word)
+blank = '_'
+print_blank = blank * len(word)
+blank_list = list(print_blank)
 
 while(number_of_attampt):
-    print(print_blank)
+    word_char_list = list(word)
+    print(word_char_list)
+    print(blank_list)
     guess_char = input("Enter Your Guess: ")
 
+# input value check loop
     while True:
         if(guess_char.isalpha() and len(guess_char) == 1):
             break
         else:
             guess_char = input("Wrong Input! Enter Your Guess: ")
-    
-    if guess_char in word:
-        for i in word_char_list:
-            if word_char_list[i] == guess_char:
-                pass
-
+    if blank in blank_list:
+        if guess_char in word_char_list:
+            for i in word_char_list:
+                print(word_char_list.index(i),i) 
+                if guess_char == i:
+                    blank_list[word_char_list.index(i)] =i
+    else:
+        print(f"You gussed the word ({word}) in number of attampt ")
+    print(blank in blank_list)
+    print(blank,blank_list)
     number_of_attampt = number_of_attampt-1
