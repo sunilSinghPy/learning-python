c90fab18dd9bb0da9b6b6d39580f0f5e4157f1f3
sunilSinghPy <coolsunilsingh@gmail.com>
2025-01-17 20:21:13 +0530

    madlib added

---
+++ madlibs.py
@@ -0,0 +1,7 @@
+#string Concatenation 
+youtuber = "suniil singh" # some string variable
+
+# a few ways to do this
+print ("subscribe to " + youtuber)
+print("subscribe to {}".format(youtuber))
+print(f"subscribe to {youtuber}") 
