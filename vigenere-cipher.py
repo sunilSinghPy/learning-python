01690a924c5b50a4defb1e657c03ad6f2e3d1ff5
sunilSinghPy <coolsunilsingh@gmail.com>
2025-02-16 22:23:17 +0530

    vigenere Cypher added

---
+++ vigenere-cipher.py
@@ -0,0 +1,27 @@
+text = "Hello World"
+custom_key = "python"
+
+def vigenere(message, key, direction=1):
+    key_index = 0
+    alphabet = 'abcdefghijklmnopqrstuvwxyz'
+    final_message = ''
+
+    for char in message.lower():
+        # Append Space to the message
+        if char == ' ':
+            final_message += char
+        else:
+            # Find the right key/char for encode/decode
+            key_char = key[key_index % len(key) ]
+            key_index +=1
+            # Define offset and encryption/decryption letter
+            offset = alphabet.index(key_char)
+            index = alphabet.find(char)
+            new_index = (index + offset * direction) % len(alphabet)
+            final_message += alphabet[new_index]
+    return final_message
+
+encryption = vigenere(text, custom_key)
+decryption = vigenere(encryption, custom_key, -1)
+print(encryption)
+print(decryption)
