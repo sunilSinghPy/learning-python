0090b5a694feb6733afa55d4e9f5722b66771852
sunilSinghPy <coolsunilsingh@gmail.com>
2025-02-20 21:56:22 +0530

    convert to snake case with list comprehension

--- convert_to_snake_case.py
+++ convert_to_snake_case.py
@@ -1,15 +1,20 @@
 def convert_to_snake_case(pascal_and_camel_case_string):
-    snake_case_char_list = []
-    for char in pascal_and_camel_case_string:
-        if char.isupper():
-            converted_charcter = '_' + char.lower()
-            snake_case_char_list.append(converted_charcter)
-        else:
-            snake_case_char_list.append(char)
+    # snake_case_char_list = []
+    # for char in pascal_and_camel_case_string:
+    #     if char.isupper():
+    #         converted_charcter = '_' + char.lower()
+    #         snake_case_char_list.append(converted_charcter)
+    #     else:
+    #         snake_case_char_list.append(char)
 
-    snake_case_string = ''.join(snake_case_char_list).strip('_')
-    print(snake_case_char_list)
-    return snake_case_string
+    # snake_case_string = ''.join(snake_case_char_list).strip('_')
+    # print(snake_case_char_list)
+    # return snake_case_string
+
+    ##### List Comprehension Method ########
+
+    snake_case_char_list = ['_'+ char.lower() if char.isupper() else char for char in pascal_and_camel_case_string]
+    return ''.join(snake_case_char_list).strip('_')
 
 def main():
     print(convert_to_snake_case("ThisIsNotCamelCase"))
